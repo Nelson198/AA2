eb9374d1dcabc2b31b593765f52c2f7c1f060db6
Rui Meira <ruimiguelcm96@gmail.com>
2020-04-10 14:54:48 +0100

    update Regression

--- Code/unicornml/regression/__init__.py
+++ Code/unicornml/regression/__init__.py
@@ -1,20 +1,24 @@
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
 from sklearn.model_selection import RandomizedSearchCV, GridSearchCV, cross_val_score
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.svm import SVR
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
 
 
 class Regression:
     def __init__(self, x_train, x_test, y_train, y_test):
-        self.x_train = x_train
-        self.x_test  = x_test
+        self.X_train = x_train
+        self.X_test  = x_test
         self.y_train = y_train
         self.y_test  = y_test
         self.methods = {
             'linear'      : self.__linearRegression,
             'poly'        : self.__polynomialRegression,
-            #'svr'         : self.__SVR,
-            #'decisionTree': self.__decisionTreeRegression,
-            #'randomForest': self.__randomForestRegression
+            'svr'         : self.__SVR,
+            'decisionTree': self.__decisionTreeRegression,
+            'randomForest': self.__randomForestRegression
         }
         self.model = {}
 
@@ -24,14 +28,76 @@ class Regression:
         return self.model
 
     def __linearRegression(self):
-        r2,regressor = self.__cross_validation(LinearRegression())
+        print("Training with Linear Regression")
+        regressor = LinearRegression()
+        regressor.fit(self.X_train, self.y_train)
+        y_pred = regressor.predict(self.X_test)
+        r2 = r2_score(self.y_test, y_pred)
+        print("Score: %f" % r2)
         if not bool(self.model) or self.model['score'] < r2:
             self.model['score'] = r2
             self.model['model'] = regressor
     
 
     def __polynomialRegression(self):
-        return 1
+        print("Training with polynomial Regression")
+        poly_reg = PolynomialFeatures( degree = 2)
+        X_poly = poly_reg.fit_transform(self.X_train)
+        poly_reg.fit(X_poly, self.y_train)
+
+        regressor = LinearRegression()
+        regressor.fit(X_poly, self.y_train)
+
+        y_pred = regressor.predict(
+            poly_reg.fit_transform(self.X_test)
+        )
+
+        r2 = r2_score(self.y_test, y_pred)
+        print("Score: %f" % r2)
+        if not bool(self.model) or self.model['score'] < r2:
+            self.model['score'] = r2
+            self.model['model'] = regressor
+
+    
+    def __SVR(self):
+        print("Training with Support Vector Regressor")
+        regressor = SVR(kernel='rbf', gamma = 'scale')
+        regressor.fit( self.X_train, self.y_train)
+        y_pred = regressor.predict(self.X_test)
+
+        r2 = r2_score(self.y_test, y_pred)
+        print("Score: %f" % r2)
+        if not bool(self.model) or self.model['score'] < r2:
+            self.model['score'] = r2
+            self.model['model'] = regressor
+        
+
+    def __decisionTreeRegression(self):
+        print("training with Decision Tree Regressor")
+        regressor = DecisionTreeRegressor()
+        regressor.fit( self.X_train, self.y_train)
+        y_pred = regressor.predict(self.X_test)
+
+        r2 = r2_score(self.y_test, y_pred)
+        print("Score: %f" % r2)
+        if not bool(self.model) or self.model['score'] < r2:
+            self.model['score'] = r2
+            self.model['model'] = regressor
+
+
+    def __randomForestRegression(self):
+        print("Training with Random Forest Regressor")
+        regressor = RandomForestRegressor(n_estimators=10)
+        regressor.fit( self.X_train, self.y_train)
+        y_pred = regressor.predict(self.X_test)
+
+        r2 = r2_score(self.y_test, y_pred)
+        print("Score: %f" % r2)
+        if not bool(self.model) or self.model['score'] < r2:
+            self.model['score'] = r2
+            self.model['model'] = regressor
+
+        
 
     def __cross_validation(self, model):
         scores = cross_val_score(
