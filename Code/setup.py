60089b7cbc1893210c8d1c09874a82bd80f1fc43
Nelson198 <nteixeira88@gmail.com>
2020-05-05 21:54:36 +0100

    #60 Commit: Pequenas correções - UnicornML

--- Code/setup.py
+++ Code/setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup, find_packages
 
-setup(name='unicornml',
-      version='0.1',
-      description='An AutoML framework',
-      #url='http://github.com/storborg/funniest',
-      author='João Imperadeiro, José Boticas, Nelson Teixeira, Rui Meira',
-      #author_email='flyingcircus@example.com',
-      license='MIT',
-      packages=find_packages(),
-      zip_safe=False,
-      test_suite="tests"
+setup(name = "unicornml",
+      version = "0.1",
+      description = "An AutoML framework",
+      # url = "http://github.com/storborg/funniest",
+      author = "João Imperadeiro, José Boticas, Nelson Teixeira, Rui Meira",
+      #author_email = "flyingcircus@example.com",
+      license = "MIT",
+      packages = find_packages(),
+      zip_safe = False,
+      test_suite = "tests"
 )
 
 #def module_tests():
 #    test_loader = unittest.TestLoader()
-#    test_suite  = test_loader.discover('tests', pattern='*.py')
+#    test_suite  = test_loader.discover("tests", pattern="*.py")
 #    return test_suite
