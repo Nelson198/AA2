08ec4ca80b5173c51f796cd3f5766824d195f0f0
Rui Meira <ruimiguelcm96@gmail.com>
2020-05-05 13:28:40 +0100

    read the yaml and detect the problem(need improvement)

--- Code/unicornml/__init__.py
+++ Code/unicornml/__init__.py
@@ -1,14 +1,25 @@
 import sys
+import yaml
 from .regression     import Regression
 from .classification import Classification
 
 
 class UnicornML:
-    def __init__(self, x_train=None, x_test=None, y_train=None, y_test=None):
-        if x_train == None or x_test == None:
-            sys.exit("Undefined data to train, val and test")
-        
-        return 1
+    def __init__(self, X, y, options = {}):
+
+        config = None
+        with open('options.yaml') as file:
+            config = yaml.full_load(file)
+        print(config, type(config))
+
+        self.__params = {}
+        if "Problem" in options:
+            if options["Problem"] not in config["Problem"]:
+                sys.exit("Invalid problem defined! Just accepting [%s]" % ' ,'.join(config["Problem"]))
+            self.__params["Problem"] = options["Problem"]
+        else:
+            # Check if it's a classification or regression problem
+            self.__params["Problem"] = self.__detect_problem(y.tolist())
         # it's unsupervised learning
         '''if bool(re.search('^int', str(y_train.dtype))):
             self.model = Classification(
@@ -20,9 +31,12 @@ class UnicornML:
                 x_train, x_test,
                 y_train, y_test
         )  '''
-    
-    # the Odin is the god model!!!
-    #def Build(self):
-    #    return self.model.Odin()
 
-        
+    #TODO improve
+    def __detect_problem(self, y):
+        # Just ints -> Classification
+        if len(y) == len([v for v in y if isinstance(v,int)]):
+            return "Classification"
+        # If they are floats -> Regression
+        else:
+            return "Regression"
