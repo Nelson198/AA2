87ce09bbc8e143f7d355aa01f4f7ec74d8e5f23e
JRI98 <38755101+JRI98@users.noreply.github.com>
2020-06-19 16:20:04 +0100

    Small fix

--- Code/unicornml/preprocessing/__init__.py
+++ Code/unicornml/preprocessing/__init__.py
@@ -23,7 +23,7 @@ def scaling_normalize_y(y):
     problem = None
     if any([not is_digit(v) for v in y]):
         problem = "Classification"
-        new_y = LabelEncoder.fit_transform(y.reshape(-1,1))
+        new_y = LabelEncoder().fit_transform(y.reshape(-1,1))
     elif all([isinstance(v,np.int64) or isinstance(v,np.int32) for v in y]):
         problem = "Classification"
         new_y = y
