146c8245e2ccba39686791aec7bc30a4914e370b
SacitobJose <zezinhoboticas@gmail.com>
2020-06-23 13:19:41 +0100

    image testing

---
+++ Code/tests/00.py
@@ -0,0 +1,19 @@
+import unittest
+import numpy as np
+from unicornml import UnicornML
+
+class TestStringMethods(unittest.TestCase):
+    def test_linearRegression(self):
+        unicorn = UnicornML(
+            {"images": "/home/joseb/Desktop/aa2/aula6/cats_and_dogs_small"},
+            {"height": 150, "width": 150, "depth": 3, "fine_tuning": False}
+        )
+        unicorn.Rainbow()
+        acc = unicorn.evaluate(1, 1)
+        print("acc: %f" % acc)
+
+        self.assertEqual("foo".upper(), "FOO")
+
+
+if __name__ == "__main__":
+    unittest.main()
